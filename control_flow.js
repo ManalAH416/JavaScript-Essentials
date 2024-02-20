4fb265df5ea70ca8070de8e49cfa673244a9e2b1
ManalAH416 <ManalAH416@gmail.com>
2024-02-20 12:31:39 +0300

    control_flow.js first update

--- control_flow.js
+++ control_flow.js
@@ -10,3 +10,18 @@ if (userRole === "admin") {
 }
 
 console.log("Access Level:", accessLevel);
+
+let isLoggedIn = true;
+let userMessage;
+
+if (isLoggedIn) {
+    if (userRole === "admin") {
+        userMessage = "Welcome, Admin!";
+    } else {
+        userMessage = "Welcome, User!";
+    }
+} else {
+    userMessage = "Please log in to access the system.";
+}
+
+console.log("User Message:", userMessage);
