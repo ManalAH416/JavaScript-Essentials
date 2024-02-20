1fe14cc90eee8497a2ae71c7173b81e083bde1fc
ManalAH416 <ManalAH416@gmail.com>
2024-02-20 12:37:04 +0300

    control_flow.js second update

--- control_flow.js
+++ control_flow.js
@@ -25,3 +25,20 @@ if (isLoggedIn) {
 }
 
 console.log("User Message:", userMessage);
+
+let userType = "subscriber";
+let userCategory;
+
+switch (userType) {
+    case "admin":
+        userCategory = "Administrator";
+        break;
+    case "manager":
+        userCategory = "Manager";
+        break;
+    case "subscriber":
+        userCategory = "Subscriber";
+        break;
+    default:
+        userCategory = "Unknown";
+}
